22209c88795c0fb5ee6d182a406cb4f0cc9a0e52
Doan Hoang Huy <d13ht01@gmail.com>
2016-04-06 08:28:23 +0700

    Initial Solution

---
+++ ScheduleManagementUsingWFA/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ScheduleManagementUsingWFA
+{
+    partial class frm_Chinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_Chinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 697);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1366, 768);
+            this.MinimumSize = new System.Drawing.Size(1364, 736);
+            this.Name = "frm_Chinh";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xếp lịch làm việc";
+            this.Load += new System.EventHandler(this.frm_Chinh_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
+
---
+++ ScheduleManagementUsingWFA/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScheduleManagementUsingWFA
+{
+    public partial class frm_Chinh : Form
+    {
+        public frm_Chinh()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Chinh_Load(object sender, EventArgs e)
+        {
+            frm_Chinh frm = new frm_Chinh();
+            frm.Width = 1024;
+            frm.Height = 768;
+        }
+    }
+}
