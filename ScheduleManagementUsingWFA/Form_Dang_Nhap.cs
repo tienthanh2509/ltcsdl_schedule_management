b9d7d84d1a60567795d3f4ff69169b880947ae1a
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2016-04-06 09:39:59 +0700

    Form Đăng Nhập

---
+++ ScheduleManagementUsingWFA/Form_Dang_Nhap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScheduleManagementUsingWFA
+{
+    public partial class Form_Dang_Nhap : Form
+    {
+        public Form_Dang_Nhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
